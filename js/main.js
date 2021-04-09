4f83c7933c2a772bb53a8a7b2f99da0455046604
Dmitriy <62738092+silver570@users.noreply.github.com>
2021-04-09 19:45:00 +0300

    Составлена структура проекта и главный экран

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper('.channel-slider', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 6,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.channel-button-next',
+      prevEl: '.channel-button-prev',
+    },
+  
+  });
