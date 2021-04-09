bd54b74adc04ec3b6bbb3161a5d2e283db3b471b
Dmitriy <62738092+silver570@users.noreply.github.com>
2021-04-09 21:07:50 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,8 +1,25 @@
 const swiper = new Swiper('.channel-slider', {
     // Optional parameters
     loop: true,
-    slidesPerView: 6,
-  
+    slidesPerView: 1,
+    spaceBetween: 20,
+    breakpoints: {
+      1900: {
+        slidesPerView: 6
+      },
+      1600: {
+        slidesPerView: 5
+      },
+      1300: {
+        slidesPerView: 4
+      },
+      1100: {
+        slidesPerView: 3
+      },
+      800: {
+        slidesPerView: 2
+      },
+    },
     // Navigation arrows
     navigation: {
       nextEl: '.channel-button-next',
@@ -10,3 +27,14 @@ const swiper = new Swiper('.channel-slider', {
     },
   
   });
+
+const searchBtn = document.querySelector('.mobile-search');
+const mobileSearch = document.querySelector('.input-group');
+searchBtn.addEventListener('clik', () => {
+  mobileSearch.classList.toggle('is-open');
+}); 
+
+
+if (document.documentElement.scrollWidth <= 640) {
+  swiper.destroy();
+}
